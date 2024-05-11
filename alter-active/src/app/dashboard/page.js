dcdcaa18409df2103ecf0eeebeb95046c0ba6a55
Taha <tahamulla@hotmail.com>
2024-05-10 22:52:06 -0400

    Fixed bug that stopped the pages from rendering

--- alter-active/src/app/dashboard/page.js
+++ alter-active/src/app/dashboard/page.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Dashboard() {
   return (
-    <div>
+    <div className='bg-black'>
        <h1>Dashboard </h1>
     </div>
   )
--- alter-active/src/app/page.tsx
+++ alter-active/src/app/page.tsx
@@ -2,6 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    
+    <div>
+      <h1>Welcome to my Next.js app!</h1>
+      <p>This is a sample home page.</p>
+    </div>
   );
 }
