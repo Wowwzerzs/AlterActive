db492c8c7ce7a5a842e8e6755a37e48548ce5ba0
Taha <tahamulla@hotmail.com>
2024-05-16 12:11:36 -0400

    Updated file

--- alter-active/src/app/layout.tsx
+++ alter-active/src/app/layout.tsx
@@ -1,10 +1,11 @@
 "use client"
-import type { Metadata } from "next";
+// import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Footer from "./components/Footer";
 import { MegaMenuDefault } from "./components/Navbar"
 import { ThemeProvider } from "@material-tailwind/react";
+
 
 
 const inter = Inter({ subsets: ["latin"] });
