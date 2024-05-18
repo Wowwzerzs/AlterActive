ea65bdd0987d0f69c788043d1f6f2cfc05c1f90c
Peter Hashem <peterhashem@gmail.com>
2024-05-18 16:09:38 -0400

    refactored

--- alter-active/src/app/dashboard/page.js
+++ alter-active/src/app/dashboard/page.js
@@ -5,17 +5,15 @@ import { useSession } from "next-auth/react";
 export default function Dashboard() {
   const { data: session, status } = useSession();
 
-  if (status === "authenticated") {
+  /*if (status === "authenticated") {
     return <p>Signed in as {session.user.email}</p>;
   }
 
-  return <a href="/api/auth/signin">Sign in</a>;
+  return <a href="/api/auth/signin">Sign in</a>; */
 
   return (
     <div className="bg-black">
-      <h1>Dashboard </h1>
+      <h1>Dashboard: signed in as {session.user.email} </h1>
     </div>
   );
-
-  
 }
