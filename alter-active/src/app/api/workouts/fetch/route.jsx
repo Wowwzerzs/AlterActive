a4c347dc2a19e7c8733cef814bd0fe82784ed4dd
Peter Hashem <peterhashem@gmail.com>
2024-05-18 23:44:12 -0400

    add route for getting workouts

---
+++ alter-active/src/app/api/workouts/fetch/route.jsx
@@ -0,0 +1,16 @@
+import { PrismaClient } from "@prisma/client";
+import { NextResponse } from "next/server";
+
+const prisma = new PrismaClient();
+
+export async function GET() {
+  try {
+    const workouts = await prisma.exercises.findMany();
+    return NextResponse.json(workouts, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Unable to fetch workouts." },
+      { status: 500 }
+    );
+  }
+}
