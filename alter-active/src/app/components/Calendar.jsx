f375c8ce2091b645294e4c275c594f33c16050e6
Taha <tahamulla@hotmail.com>
2024-05-19 15:28:52 -0400

    Added a calendar component

---
+++ alter-active/src/app/components/Calendar.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Calendar as ReactCalendar } from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+
+const Calendar = ({ workoutData, onDateClick }) => {
+  const tileContent = ({ date, view }) => {
+    if (view === 'month') {
+      const dateString = date.toISOString().split('T')[0];
+      const workoutsForDate = (workoutData || []).filter(workout => workout.date === dateString);
+      if (workoutsForDate.length > 0) {
+        return (
+          <ul>
+            {workoutsForDate.map((workout, index) => (
+              <li key={index} className="text-black">
+                {workout.muscleGroup}
+              </li>
+            ))}
+          </ul>
+        );
+      }
+    }
+    return null;
+  };
+
+  return (
+    <div className="mx-auto bg-white p-6 rounded shadow-md text-black">
+      <ReactCalendar
+        tileContent={tileContent}
+        onClickDay={onDateClick}
+      />
+    </div>
+  );
+};
+
+export default Calendar;
