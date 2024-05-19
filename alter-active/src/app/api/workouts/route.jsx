9c4e8b50b0d7fed035eefaa6f3029a7b65d359d9
Peter Hashem <peterhashem@gmail.com>
2024-05-18 23:43:51 -0400

    deleted unused file

--- alter-active/src/app/api/workouts/route.jsx
+++
@@ -1,8 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
-
-export async function GET(request) {
-  const workouts = await prisma.exercises.findMany();
-  
-};
